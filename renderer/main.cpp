ef783c413b0b06299a392605828191627d02958a
nyan-ko <dumbstupidcatboy@gmail.com>
2024-05-29 16:29:42 -0400

    rename renderer fldr

--- renderer/main.cpp
+++ renderer/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
     TGAImage image(100, 100, TGAImage::RGB);
-    TGAColor color(255, 255, 255, 255);
+    TGAColor color(255, 100, 100, 255);
     // line(0, 0, 20, 40, image, color);
     triangle(40, 50, 10, 10, 70, 20, image, color);
 
